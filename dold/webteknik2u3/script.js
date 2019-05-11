dcb9885aff8090c27ab26dd7da98b08a43b939ec
Rickard Marjanovic <rickard.marjanovic@gmail.com>
2019-05-11 14:05:46 +0200

    Trying to get Cookies to work

--- dold/webteknik2u3/script.js
+++ dold/webteknik2u3/script.js
@@ -10,9 +10,18 @@ var picIndex;           //Flagga för att hålla koll på valt index
 var checkTurns;         //Håller koll på antalet omgångar (för poäng)
 var rightPicks;         //Håller koll på antalet rätta val
 var showTurns;          //Referens till span element avs antalet turns.
+var userTotPoints;      //Referens till span element avs totala poängen.
+var userCountGames;     //Referens till span element avs antalet spel.
+var userMeanPoints;     //Referens till span element avs medelpoängen.
+var totalGames;         //Variabel som håller koll på antalet spel
+var totalPoints;        //Variabel som håller koll på totala poängen
+var meanScore;          //Variabel som håller koll på medelpoängen
 
 // Initiera globala variabler och koppla funktion till knapp
 function init() {
+    totalPoints=0;
+    totalGames=0;
+    meanScore=0;
     picIndex = [];          //Skapar en array
     selectedPicture = [];   //Skapar en array
     picTiles=[];            //Skapar en array
@@ -21,14 +30,22 @@ function init() {
     nextButton=document.getElementById("nextBtn");
     picsElems=document.getElementById("bricks").getElementsByTagName("img");
     showTurns=document.getElementById("turnNr");
+    userTotPoints=document.getElementById("userTotPoints");
+    userCountGames=document.getElementById("userCountGames");
+    userMeanPoints=document.getElementById("userMeanPoints");
+    //getCookie();            //Hämtar cookie värden
     addListener(startGameBtn,"click", startGame);
     addListener(nextButton,"click", checkPick);
     nextButton.disabled=true;
+
 } //End init
 addListener(window,"load",init); // Se till att init aktiveras då sidan är inladdad
+addListener(window, "unload", saveCookie);
 
 //Funktion för att initiera spel
 function startGame() {
+    //getCookie();            //Hämtar cookie värden
+    totalGames++;   //Antal spel ökar med 1
     startGameBtn.disabled=true;
     checkTurns=0;   //Nytt spel, noll omgångar
     rightPicks=0;   //Nytt spel, noll rätt
@@ -44,7 +61,7 @@ function startGame() {
     //Event för klick samt ett unikt attribut för att hitta bilder
     for(i=0;i<picsElems.length;i++) {
         addListener(picsElems[i], "click", turnPic);
-        picsElems[i].setAttribute("index", i)
+        picsElems[i].setAttribute("index", i) //Sätt att hantera turnpic, länka bilder till tiles
     }
 } //End startGame
 
@@ -89,14 +106,14 @@ function turnPic() {
     if(noTurns>=2) {  
         return;
     }    
-    console.log("turnPic", noTurns);
+    console.log("turnPic", noTurns); //Använde consol log för att felsöka hantera.
     
-    picIndex[noTurns]=this.getAttribute("index");
+    picIndex[noTurns]=this.getAttribute("index"); //Sätt att hantera turnpic, länka bilder till tiles
     picsElems[picIndex[noTurns]].src="pics/"+picTiles[picIndex[noTurns]]+".png"
     picsElems[picIndex[noTurns]].className="brickFront";
     selectedPicture[noTurns]=picsElems[picIndex[noTurns]].src
     
-    console.log("picIndex", picIndex[noTurns]);
+    console.log("picIndex", picIndex[noTurns]); //Använde consol log för att felsöka hantera.
 
 
     if(picIndex[1]!=picIndex[0]) {
@@ -115,8 +132,8 @@ function turnPic() {
 
 function checkPick() {
     noTurns=0;
-    console.log("Checkpick");
-    if (selectedPicture[0]==selectedPicture[1]) {
+    console.log("Checkpick"); //Använde consol log för att felsöka hantera.
+    if (selectedPicture[0]==selectedPicture[1]) { //bugg avs att man kunde klicka på samma bild två ggr och få upp nästa
         picsElems[picIndex[0]].src="pics/empty.png";
         picsElems[picIndex[1]].src="pics/empty.png";
         picsElems[picIndex[0]].className="brickEmpty";
@@ -129,8 +146,8 @@ function checkPick() {
         picsElems[picIndex[1]].src="pics/backside.png"
         picsElems[picIndex[0]].className="brickBack";
         picsElems[picIndex[1]].className="brickBack";
-        picIndex[0]=null;
-        picIndex[1]=null;
+        picIndex[0]=null; //Felsökte stor bugg, om man klickade på samma första gången gick det att vända tre bilder. Löstes genom denna kodrad.
+        picIndex[1]=null; //Felsökte stor bugg, om man klickade på samma första gången gick det att vända tre bilder. Löstes genom denna kodrad.
     }
     checkTurns++;
     for(i=0;i<picsElems.length;i++) {
@@ -142,10 +159,62 @@ function checkPick() {
 } //End checkPick
 
 function checkWin() {
+    if(rightPicks==picsElems.length) {
+        picIndex[0]=null; //Felsökte stor bugg, om man klickade på samma första gången gick det att vända tre bilder. Löstes genom denna kodrad.
+        picIndex[1]=null; //Felsökte stor bugg, om man klickade på samma första gången gick det att vända tre bilder. Löstes genom denna kodrad.
+        var points=20-Math.round((checkTurns-rightPicks/2)*1.2); //lokalvariabel för att räkna rundans poäng
+        
+        alert(points);
 
+
+        //Om man får ett belopp under 0 ska poängen sättas till noll
+        if (points<0) {
+            points=0;
+        }
+        
+        totalPoints+=points; //adderar rundans poäng till totalen
+        
+        //Uppdaterar användarens vy med poänginfo
+        meanScore=totalPoints/totalGames;
+        userCountGames.innerHTML=totalGames;
+        userMeanPoints.innerHTML=meanScore;
+        userTotPoints.innerHTML=totalPoints;
+
+        nextButton.disabled=true;
+        startGameBtn.disabled=false;
+    }
 } //End checkWin
 
-//Felsökte stor bugg, om man klickade på samma första gången.
-//Använde consol log för att felsöka hantera.
-//Sätt att hantera turnpic, länka bilder till tiles
-//bugg avs att man kunde klicka på samma bild två ggr och få upp nästa
+//Funktion för att spara diverse värden i cookies
+function saveCookie() {
+    setCookie("totalScore", totalPoints);
+    setCookie("totalGames", totalGames);
+    setCookie("meanScore", meanScore);
+}
+
+//Funktion för att hämta värden från cookies och sparas i globala variabler
+function getCookie() {
+    totalPoints=getCookie("totalScore");
+    totalGames=getCookie("totalGames");
+    meanScore=getCookie("meanScore");
+    if (totalPoints!=null) {
+        userTotPoints=totalPoints;
+    } else {
+        totalPoints=0;
+    }
+    if (userCountGames!=null) {
+        userCountGames=totalGames;
+    } else {
+        userCountGames=0;
+    }
+    if (userMeanPoints!=null) {
+        userMeanPoints=meanScore;
+    } else {
+        userMeanPoints=0;
+    }
+}
+
+
+
+
+
